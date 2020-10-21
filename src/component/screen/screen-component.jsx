5a27d76cad632b23749d872803b432cde4ad0649
Hetvi <hetvishah2512@gmail.com>
2020-10-22 01:35:24 +0530

    created a simple calculator in react

---
+++ src/component/screen/screen-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './screen.styles.css'
+const Screen = ({input}) => {
+return (
+<div className="screen">
+
+    <div>{input}</div>
+</div>
+);
+};
+
+export default Screen;
